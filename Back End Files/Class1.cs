1478ef44f44dec88cb0e58579f7fe18a4e131828
Mario <MartinezM9697@UHCL.edu>
2023-02-09 09:11:58 -0600

    NA

---
+++ Back End Files/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_Risks_UHCL_Agile.Back_End_Files
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Front End Files/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical_Risks_UHCL_Agile.Front_End_Files
+{
+    internal class Class2
+    {
+    }
+}
